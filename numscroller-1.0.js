3bec59e04848e30bdbe86f2005970b33efc1d21c
Skid Vis <skid@skidvis.com>
2017-06-06 14:14:28 -0500

    Allow setting timespan.  … Remove zipscroll section. Automatically sets minimum. Correctly sets duration based on data-delay.

--- numscroller-1.0.js
+++ numscroller-1.0.js
@@ -1,88 +1,57 @@
 /**
-* jQuery scroroller Plugin 1.0
+* jQuery scroroller Plugin 1.x
 *
 * http://www.tinywall.net/
 * 
 * Developers: Arun David, Boobalan
-* Copyright (c) 2014 
+* Copyright (c) 2014
+* Heavily modified by Skid Vis
 */
 (function($){
     $(window).on("load",function(){
-        $(document).scrollzipInit();
         $(document).rollerInit();
     });
-    $(window).on("load scroll resize", function(){
-        $('.numscroller').scrollzip({
-            showFunction    :   function() {
-                                    numberRoller($(this).attr('data-slno'));
-                                },
-            wholeVisible    :     false,
-        });
-    });
-    $.fn.scrollzipInit=function(){
-        $('body').prepend("<div style='position:fixed;top:0px;left:0px;width:0;height:0;' id='scrollzipPoint'></div>" );
-    };
     $.fn.rollerInit=function(){
         var i=0;
         $('.numscroller').each(function() {
             i++;
            $(this).attr('data-slno',i); 
            $(this).addClass("roller-title-number-"+i);
+           numberRoller($(this).attr('data-slno'));
         });        
     };
-    $.fn.scrollzip = function(options){
-        var settings = $.extend({
-            showFunction    : null,
-            hideFunction    : null,
-            showShift       : 0,
-            wholeVisible    : false,
-            hideShift       : 0,
-        }, options);
-        return this.each(function(i,obj){
-            $(this).addClass('scrollzip');
-            if ( $.isFunction( settings.showFunction ) ){
-                if(
-                    !$(this).hasClass('isShown')&&
-                    ($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.showShift)>($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))&&
-                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))<($(this).outerHeight()+$(this).offset().top-settings.showShift)
-                ){
-                    $(this).addClass('isShown');
-                    settings.showFunction.call( this );
-                }
-            }
-            if ( $.isFunction( settings.hideFunction ) ){
-                if(
-                    $(this).hasClass('isShown')&&
-                    (($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.hideShift)<($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))||
-                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))>($(this).outerHeight()+$(this).offset().top-settings.hideShift))
-                ){
-                    $(this).removeClass('isShown');
-                    settings.hideFunction.call( this );
-                }
-            }
-            return this;
-        });
-    };
     function numberRoller(slno){
-            var min=$('.roller-title-number-'+slno).attr('data-min');
-            var max=$('.roller-title-number-'+slno).attr('data-max');
-            var timediff=$('.roller-title-number-'+slno).attr('data-delay');
-            var increment=$('.roller-title-number-'+slno).attr('data-increment');
-            var separator=$('.roller-title-number-'+slno).attr('data-separator');
-            separator = (separator === undefined) ? '' : separator;
-            var numdiff=max-min;
-            var timeout=(timediff*1000)/numdiff;
-            numberRoll(slno,min,max,increment,timeout,separator);
+            var el = {
+                id : slno,
+                minAmount : 0,
+                maxAmount : $('.roller-title-number-' + slno).attr('data-max') *1,
+                differenceAmount : 0,                                
+                incrementAmount : 0,
+                tickCount : 0,
+                thousandsSeparator : $('.roller-title-number-'+slno).attr('data-separator'),
+                secondsToComplete : $('.roller-title-number-'+slno).attr('data-delay') * 1,
+                speed : 30,
+                totalMilliseconds : 0         
+            };
+
+            el.thousandsSeparator = (el.thousandsSeparator === undefined) ? '' : el.thousandsSeparator;
+            el.totalMilliseconds = el.secondsToComplete * 1000;
+            el.tickCount = el.totalMilliseconds / el.speed;
+            el.minAmount = el.maxAmount - (el.maxAmount * .10);            
+            el.differenceAmount = el.maxAmount - el.minAmount;
+            el.incrementAmount = el.differenceAmount / el.tickCount;            
+
+            numberRoll(el);
     }
-    function numberRoll(slno,min,max,increment,timeout, separator){
-        if(min<=max){
-            var value = addSeparator(min, separator);
-            $('.roller-title-number-'+slno).html(value);
-            min=parseInt(min)+parseInt(increment);
-            setTimeout(function(){numberRoll(eval(slno),eval(min),eval(max),eval(increment),eval(timeout), separator)},timeout);
+    function numberRoll(el){
+        if(el.minAmount<=el.maxAmount){
+            var value = addSeparator(el.minAmount, el.thousandsSeparator);
+            $('.roller-title-number-'+el.id).html(value);
+            el.minAmount=parseInt(el.minAmount)+parseInt(el.incrementAmount);
+            setTimeout(function(){numberRoll(el)},el.speed);
         }else{
-            var value = addSeparator(max, separator);
-            $('.roller-title-number-'+slno).html(value);
+            var value = addSeparator(el.maxAmount, el.thousandsSeparator);
+            $('.roller-title-number-'+el.id).html(value);
         }
     }
     function addSeparator(nStr, separator) {
