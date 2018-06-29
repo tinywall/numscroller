bd51d54f1bc3bc2ec4363adaafc2f0581ce40e24
Eleandro Duzentos <eleandro@inbox.ru>
2018-06-29 22:48:04 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+let gulp         = require('gulp'),
+    uglify       = require('gulp-uglify')
+    rename       = require('gulp-rename')
+    sass         = require('gulp-sass')
+    autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('build', () => {
+  return gulp.src('./src/scroll-incr.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('sass', () => {
+    return gulp.src('./demo/sass/main.sass')
+        .pipe(sass())
+        .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+        .pipe(gulp.dest('./demo/css'))
+})
---
+++ src/scroll-incr.js
@@ -0,0 +1,168 @@
+/**
+ * Scroll-Incr
+ *
+ * https://e200.github.io/
+ * 
+ * Developers: Eleandro Duzentos, Arun David, Boobalan
+ * Copyright (c) 2018
+ */
+(function ($) {
+  /**
+   * Represents each incrementable
+   * element and its respective data.
+   */
+  var incrBag          = [],
+      w                = $(window),
+      incrElementsLeft = 0
+
+  /**
+   * Once the page is loaded,
+   * we initialize the plugin.
+   */
+  w.on('load', function() {
+    $('.scroll-incr').initIncrEl()
+  })
+
+  function scrollIncrHandler () {
+    $('.scroll-incr').visibleOnViewport({
+      /**
+       * This function will be trigged when
+       * the element become visible on the viewport.
+       */
+      showFunction: function () {
+        /**
+         * Lets take the id of the current
+         * element, because `number`, because
+         * `incrementor` needs it.
+         */
+        var incrElementId = $(this).attr('data-incr-id')
+    
+        incrementor(incrElementId)
+      }
+    })
+  }
+
+  w.on('load scroll resize', scrollIncrHandler)
+  
+  $.fn.initIncrEl = function () {
+    var incrElements = $(this)
+
+    incrElementsLeft = incrElements.length
+    
+    incrElements.each(function (index, incrEl) {
+      var incrEl = $(incrEl)
+
+      /**
+       * Adding an identifier for
+       * the current element.
+       * 
+       * Will use it later to identy
+       * it, and start the incrementation
+       * process.
+       */
+      incrEl.attr('data-incr-id', index)
+
+      /**
+       * Here we're getting each
+       * data needed to perform the
+       * incrementation.
+       * 
+       * SOme of them are provided
+       * using data-attributes.
+       * 
+       * They have default values,
+       * except the `data-max` attribute
+       * that must be present on the `incrEl`.
+       */
+      var min       = Number(incrEl.attr('data-min')) || 0,
+          max       = Number(incrEl.attr('data-max')),
+          timediff  = Number(incrEl.attr('data-dincrElay')) || 4,
+          increment = Number(incrEl.attr('data-increment')) || 1,
+          numdiff   = max - min
+    
+      var incrData = {
+        el:        incrEl,
+        min:       min,
+        max:       max,
+        timediff:  timediff,
+        increment: increment,
+        numdiff:   numdiff,
+        timeout:   (timediff * 1000) / numdiff
+      }
+  
+      // Lets put the data on the bag.
+      incrBag.push(incrData)
+    })
+  }
+
+
+  /**
+   * Checks if the `incrElement`
+   * is visible on the viewport
+   */
+  function isVisible(incrElement) {
+    return w.scrollTop() > ($(incrElement).offset().top - window.innerHeight)
+  }
+
+  /**
+   * Checks if the `incrElement`
+   * was touched (incremented)
+   */
+  function isTouched(i) {
+    return incrBag[i].touched
+  }
+
+  $.fn.visibleOnViewport = function (options) {
+    if (incrElementsLeft > 0) {
+      this.each(function (i, incrElement) {
+        /**
+         * If the element wasn't touched
+         * and is visible, lets mark it as
+         * touched and call `showFunction`.
+         */
+        if (!isTouched(i) && isVisible(incrElement)) {
+          incrElementsLeft--
+          incrBag[i].touched = true
+          
+          options.showFunction.call(this)
+        }
+      })
+    } else {
+      /**
+       * All `.scroll-incr` elements were
+       * processed.
+       * 
+       * Lets unbind our listeners.
+       */
+      w.off('load scroll resize', scrollIncrHandler)
+    }
+
+    return this
+  }
+  
+  function incrementor(incrId) {
+    var currentIncrBag = incrBag[incrId],
+  
+    el        = currentIncrBag.el,
+    min       = currentIncrBag.min,
+    max       = currentIncrBag.max,
+    increment = currentIncrBag.increment,
+    timeout   = currentIncrBag.timeout
+  
+    realIncrementor(el, min, max, increment, timeout)
+  }
+  
+  function realIncrementor(el, min, max, increment, timeout) {
+    if (min <= max) {
+      el.html(min)
+    
+      min = min + increment
+    
+      setTimeout(function () {
+        realIncrementor(el, min, max, increment, timeout)
+      }, timeout)
+    } else {
+      el.html(max)
+    }
+  }
+})(jQuery)
